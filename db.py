8d92bf7724f870e1147db85ec6fdbda3c9e25b08
Manasa M <manasamahadev016@gmail.com>
2026-02-07 12:31:21 +0530

    commit

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL",DATABASE_URL)
 
 #create engine once at module level
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+engine = create_engine(DATABASE_URL)
 
 #create sessionLocal class for creating sessions
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
