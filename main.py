5d5d27e6425a5b94562be514d982a319d8514917
Manasa M <manasamahadev016@gmail.com>
2026-04-25 19:46:38 +0530

    Add Vercel frontend URL to CORS origins

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ app = FastAPI()
 #cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173", 
+        "http://localhost:3000",
+        "https://lumina-black-eta.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
